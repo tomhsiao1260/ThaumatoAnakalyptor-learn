4795f2355c00353d3500b86abe8521611531e466
Hsiao Yao <tomtogo1260@gmail.com>
2024-04-04 22:42:04 +0800

    grid to pointcloud init

--- ThaumatoAnakalyptor/grid_to_pointcloud.py
+++ ThaumatoAnakalyptor/grid_to_pointcloud.py
@@ -1,7 +1,18 @@
 ### Julian Schilliger - ThaumatoAnakalyptor - Vesuvius Challenge 2023
 
+CFG = {'num_threads': 4, 'GPUs': 1}
+
+def compute():
+    pass
+
 def main():
-    print('Hello World')
+    base_path = ""
+    volume_subpath = "../2dtifs_8um_grids"
+    pointcloud_subpath = "../point_cloud"
+    num_threads = CFG['num_threads']
+    gpus = CFG['GPUs']
+
+    compute()
 
 if __name__ == "__main__":
     main()
