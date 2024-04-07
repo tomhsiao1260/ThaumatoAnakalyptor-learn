13c4f53e778ab16f0837097458f932f2fed509fc
Hsiao Yao <tomtogo1260@gmail.com>
2024-04-07 14:30:46 +0800

    complete blur part

--- ThaumatoAnakalyptor/surface_detection.py
+++ ThaumatoAnakalyptor/surface_detection.py
@@ -1,10 +1,43 @@
 import torch
+import torch.nn as nn
+
 import tifffile
 import numpy as np
 
+# Function to create a 3D Uniform kernel
+def get_uniform_kernel(size=3, channels=1):
+    # Create a 3D kernel filled with ones and normalize it
+    kernel = torch.ones((size, size, size))
+    kernel = kernel / torch.sum(kernel)
+    return kernel
+
+# Function to create a 3D convolution layer with a Uniform kernel
+def uniform_blur3d(channels=1, size=3, device=None):
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    kernel = get_uniform_kernel(size, channels)
+    # Repeat the kernel for all input channels
+    kernel = kernel.repeat(channels, 1, 1, 1, 1)
+    # Create a convolution layer
+    blur_layer = nn.Conv3d(in_channels=channels, out_channels=channels, 
+                           kernel_size=size, groups=channels, bias=False, padding=size//2)
+    # Set the kernel weights
+    blur_layer.weight.data = nn.Parameter(kernel)
+    # Make the layer non-trainable
+    blur_layer.weight.requires_grad = False
+    blur_layer.to(device)
+    return blur_layer
+
 # Function to detect surface points in a 3D volume
 def surface_detection(volume, global_reference_vector, blur_size=3, sobel_chunks=4, sobel_overlap=3, window_size=20, stride=20, threshold_der=0.1, threshold_der2=0.001, convert_to_numpy=True):
-    return (volume, global_reference_vector)
+    # device
+    device = volume.device
+    # using half percision to save memory
+    volume = volume
+    # Blur the volume
+    blur = uniform_blur3d(channels=1, size=3, device=device)
+    blurred_volume = blur(volume.unsqueeze(0).unsqueeze(0)).squeeze(0).squeeze(0)
+
+    return (blurred_volume, global_reference_vector)
 
 if __name__ == '__main__':
     path = '../2dtifs_8um_grids/cell_yxz_006_008_004.tif'
@@ -21,6 +54,4 @@ if __name__ == '__main__':
     volume = volume.numpy()
     volume = np.uint8(volume)
 
-    tifffile.imwrite('output.tif', volume) 
-    
-    print(volume.shape)
+    tifffile.imwrite('output.tif', volume)
--- surface_detection.md
+++ surface_detection.md
@@ -1,3 +1,7 @@
 ## surface_detection
 
-核心程式碼在 surface_detection 方法。
+核心程式碼在 surface_detection 方法。第一部會先模糊化，濾掉高頻雜訊。
+
+#### uniform_blur3d
+
+回傳一個用來模糊化的 function，是一層 3D convolution，用均勻的 kernal 達到模糊化的效果。
