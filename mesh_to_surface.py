cb59856c3da1ad8bb666b2f946cebe0b70e76145
Hsiao Yao <tomtogo1260@gmail.com>
2024-08-13 19:18:04 +0800

    mesh to surface init

---
+++ mesh_to_surface.py
@@ -0,0 +1,14 @@
+### Julian Schilliger - ThaumatoAnakalyptor - 2024
+
+import argparse
+
+def ppm_and_texture():
+  pass
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  args = parser.parse_known_args()[0]
+
+  print(f"Rendering args: {args}")
+
+  ppm_and_texture()
